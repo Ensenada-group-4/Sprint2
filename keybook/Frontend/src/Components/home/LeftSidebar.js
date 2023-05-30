e2609af9b3394d7f8d0be4580a494c83302b531a
Lu <luciasaenz92@hotmail.com>
2023-05-30 13:35:54 +0200

    leftSide OK execpt update with effect

--- keybook/Frontend/src/Components/home/LeftSidebar.js
+++ keybook/Frontend/src/Components/home/LeftSidebar.js
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../../App.css";
 import Unfollow from "../buttons/UnfollowButton";
 
 function LeftSidebar() {
-  const [users, setUsers] = useState([]);
-  const [clickedUserId, setClickedUserId] = useState(null);
-  const userId = 1;
+  const [users, setUsers] = useState([]);  
+  const userId = localStorage.getItem('userId');
+
   async function fetchUsers() {
     try {
       const response = await fetch(
         `http://localhost:3000/follow/following/${userId}`
       );
       const data = await response.json();
-      setUsers(data);
-      console.log(data);
+      setUsers(data);    
     } catch (error) {
       console.error(error);
     }
@@ -27,14 +24,14 @@ function LeftSidebar() {
     <div className="default-card-left ">
       <h2>SIGUIENDO</h2>
       {users.map((user) => (
-        <li key={user.id}>
+        <ul key={user.id}>
         <a  title={`Perfil ${user.name}`} href={`/profile/${user.id}`}>
           <img src={user.profile_picture} alt="Avatar" className="avatar" /></a>
           <li><h4>{user.name} {user.last_name}</h4></li>
           <li>           
            <Unfollow id={user.id}/>            
           </li>
-        </li>
+        </ul>
       ))}
     </div>
   );
