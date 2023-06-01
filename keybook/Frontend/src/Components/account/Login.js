1b85a7ac4c8aefbf89bcd89faf48d2b04d2795e3
Lu <luciasaenz92@hotmail.com>
2023-06-01 16:01:17 +0200

    edit halfway

--- keybook/Frontend/src/Components/account/Login.js
+++ keybook/Frontend/src/Components/account/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import request from "../../utils/url";
 import { Logo } from "../logo/Logo";
-import { FormInput } from "../registration/FormInput";
+import { FormInput } from "./FormInput";
 import { ButtonDefault } from "../buttons/ButtonDefault";
 
 export default function Login() {
