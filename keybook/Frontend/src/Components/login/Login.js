e4125de3ca864ce52bf1e0a8108595f16db90031
Lu <luciasaenz92@hotmail.com>
2023-06-01 15:53:20 +0200

    login OK

--- keybook/Frontend/src/Components/login/Login.js
+++ keybook/Frontend/src/Components/login/Login.js
@@ -7,6 +7,8 @@ import { ButtonDefault } from "../buttons/ButtonDefault";
 export default function Login() {
     const [loginInfo, setLoginInfo] = useState({ email: "", password: "" });
     const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
     function handleChange(e) {
         const { name, value } = e.target;
         setLoginInfo({ ...loginInfo, [name]: value });
@@ -23,10 +25,12 @@ export default function Login() {
                 body: { email, password },
             });
             if (response.id) {
-                alert("Usuario logueado con éxito. Redireccionando a home")
+                setSuccess(true);
                 localStorage.setItem('userId', response.id);
                 localStorage.setItem("token", response.token)
-                window.location.href = "/home";
+                setTimeout(() => {
+                    window.location.href = "/home"
+                }, 2000)
             } else {
                 setError(true);
             }
@@ -50,11 +54,6 @@ export default function Login() {
                     <div className="col-sm-5 col-md-6 col-lg-6">
                         <div className="default-card welcome-card ">
                             <h1>Inicio de Sesión</h1>
-                            {error && (
-                                <div style={{ color: "red", fontWeight: "bold", textAlign: "center" }}>
-                                    Usuario o contraseña incorrectos
-                                </div>
-                            )}
                             <form onSubmit={handleSubmit}>
                                 <FormInput
                                     type="email"
@@ -62,8 +61,9 @@ export default function Login() {
                                 <FormInput
                                     type="password"
                                     onChange={handleChange} value={loginInfo.password} name="password" placeholder="Contraseña" required />
+                                {error && (<div className="error form-control"> Usuario o contraseña incorrectos </div>)}
+                                {success && <div className="success col-md-8  form-control ">Usuario correcto ✔ Redireccionando a Home</div>}
                                 <ButtonDefault type="submit" content="Login" id="register-form-buttons" className="btn-lg" />
-
                                 <div className="d-flex justify-content-center">
                                     <p className="slogan-container">Si no tienes cuenta <a href="/register">Registrate</a></p>
                                 </div>
@@ -73,5 +73,5 @@ export default function Login() {
                 </div>
             </div>
         </>
-    );
+    )
 }
