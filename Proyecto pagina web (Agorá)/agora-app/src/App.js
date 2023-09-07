70854e46f614d221132c78add90b0564dec91c83
Stegonyrob <stegonyrob@yahoo.es>
2023-09-07 18:18:04 +0200

    primeros pasos

--- Proyecto pagina web (Agorá)/agora-app/src/App.js
+++ Proyecto pagina web (Agorá)/agora-app/src/App.js
@@ -1,104 +1,14 @@
 import "./App.css";
 import React from "react";
-import { connect } from "react-redux";
+import NavMenu from "./components/navmenu/navStart";
 
-const menuItems = [
-  { id: 1, label: "Inicio", subMenu: null },
-  { id: 2, label: "Sobre mí", subMenu: null },
-  {
-    id: 3,
-    label: "Paradigma",
-    subMenu: [
-      { id: 31, label: "Artículos sugeridos", link: "/articulos" },
-      { id: 32, label: "Actividades", link: "/actividades" },
-      { id: 33, label: "Servicios", link: "/servicios" },
-    ],
-  },
-  { id: 4, label: "Páginas sugeridas", subMenu: null },
-  { id: 5, label: "Formulario de contacto", subMenu: null },
-];
-
-const NavItem = ({ item, isActive, onMouseEnter, onMouseLeave }) => {
-  const hasSubMenu = item.subMenu !== null;
-
+function App() {
   return (
-    <li
-      className={`w-12 h-12 rounded-full flex justify-center items-center cursor-pointer transition-colors duration-300 ${
-        isActive ? "bg-gray-900" : "bg-gray-800 hover:bg-gray-700"
-      }`}
-      onMouseEnter={() => hasSubMenu && onMouseEnter(item)}
-      onMouseLeave={() => hasSubMenu && onMouseLeave(item)}
-    >
-      {item.label}
-    </li>
+    <div className="App">
+      <NavMenu />
+      <h1>Bienvenido a mi sitio web</h1>
+      <p>...</p>
+    </div>
   );
-};
-
-const SubMenuItem = ({ item }) => {
-  return (
-    <li className="p-2">
-      <a href={item.link}>{item.label}</a>
-    </li>
-  );
-};
-
-const NavSubMenu = ({ subMenu }) => {
-  return (
-    <ul className="absolute top-full left-0 z-10 bg-gray-800 p-4 rounded-lg shadow-lg">
-      {subMenu.map((item) => (
-        <SubMenuItem key={item.id} item={item} />
-      ))}
-    </ul>
-  );
-};
-
-class NavMenu extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeSubMenuId: null,
-    };
-  }
-
-  handleMouseEnter = (item) => {
-    this.setState({ activeSubMenuId: item.id });
-  };
-
-  handleMouseLeave = (item) => {
-    this.setState({ activeSubMenuId: null });
-  };
-
-  render() {
-    const { activeSubMenuId } = this.state;
-    const { activeItemId } = this.props;
-
-    return (
-      <nav className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-        <ul className="flex">
-          {menuItems.map((item) => (
-            <React.Fragment key={item.id}>
-              <NavItem
-                item={item}
-                isActive={item.id === activeItemId}
-                onMouseEnter={this.handleMouseEnter}
-                onMouseLeave={this.handleMouseLeave}
-              />
-              {item.subMenu !== null && activeSubMenuId === item.id && (
-                <NavSubMenu subMenu={item.subMenu} />
-              )}
-            </React.Fragment>
-          ))}
-        </ul>
-      </nav>
-    );
-  }
 }
-
-const mapStateToProps = (state) => {
-  return {
-    activeItemId: state.activeItemId,
-  };
-};
-
-export default connect(mapStateToProps)(NavMenu);
+export default App;
--- Proyecto pagina web (Agorá)/agora-app/src/index.js
+++ Proyecto pagina web (Agorá)/agora-app/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { configureStore } from "redux";
 import rootReducer from "./store";
 
-const store = createStore(rootReducer);
+const store = configureStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
